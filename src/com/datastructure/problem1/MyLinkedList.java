09247c6b28e677486be485a74bc9009c2aa51c7b
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-29 13:34:55 +0530

    [Vaibhav] added UnOrdered list problem is completed

--- src/com/datastructure/problem1/MyLinkedList.java
+++ src/com/datastructure/problem1/MyLinkedList.java
@@ -10,5 +10,76 @@ public class MyLinkedList<K>
 		this.head = null;
 	}
 	
+	public void add(INode myNode)
+	{
+		if(tail==null)
+			tail=myNode;
+		if(head==null)
+			head=myNode;
+		else
+		{
+			tail.setNext(myNode);
+			tail=myNode;
+		}
+	}
 	
+	
+	public boolean search(K find)
+	{
+		INode tempNode=head;
+		int flag=0;
+		while(tempNode!=null)
+		{
+			if(tempNode.getKey().equals(find))
+			{
+				return true;
+			}
+			tempNode=tempNode.getNext();
+		}
+	    return false;
+	}
+	
+	
+	public void delete(K delete)
+	{
+		INode tempNode1=head;
+		INode tempNode2=head;
+		while(!tempNode1.getKey().equals(delete))
+		{
+			tempNode2=tempNode1;
+			tempNode1=tempNode1.getNext();
+		}
+		tempNode2.setNext(tempNode1.getNext());
+		tempNode1.setNext(null);
+	}
+	
+	public void print()
+	{
+		StringBuilder sb=new StringBuilder("List = ");
+		INode tempNode=head;
+		while(tempNode!= null)
+		{
+			sb.append(tempNode.getKey());
+			if(!tempNode.equals(tail))
+				sb.append("->");
+			tempNode=tempNode.getNext();
+		}
+		System.out.println(sb);
+	}
+	public StringBuilder myText()
+	{
+		StringBuilder sb=new StringBuilder("Text = ");
+		INode tempNode=head;
+		while(tempNode!= null)
+		{
+			sb.append(tempNode.getKey());
+			if(!tempNode.equals(tail))
+				sb.append(" ");
+			tempNode=tempNode.getNext();
+		}
+		return sb;
+	}
+	public String toString() {
+        return "MyLinkedListNodes {"+ head + "}" ;
+    }
 }
--- src/com/datastructure/problem1/MyUnorderedRunner.java
+++ src/com/datastructure/problem1/MyUnorderedRunner.java
@@ -6,7 +6,28 @@ public class MyUnorderedRunner
 {
 	public static void main(String[] args) 
 	{
-		
+		String text="Do it till you get it";
+		MyLinkedList list=new MyLinkedList();
+		String []word=text.split(" ");
+		for(String str:word)
+		{
+			MyNode<String> node=new MyNode<>(str);
+			list.add(node);
+		}
+		System.out.println(list.myText());
+		System.out.println("Enter word tobe searched : ");
+		Scanner sc=new Scanner(System.in);
+		String search=sc.nextLine();
+		if(list.search(search))
+		{
+			list.delete(search);
+		}
+		else
+		{
+			MyNode<String> node=new MyNode<>(search);
+			list.add(node);
+		}
+		System.out.println(list.myText());
 	}
 
 }
